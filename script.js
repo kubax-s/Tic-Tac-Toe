8d8f116d7bc4c760af14050d63626704f82b8b4c
Jakub Solarz <solarzjakub97@gmail.com>
2020-01-31 23:56:43 +0100

    First version

---
+++ script.js
@@ -0,0 +1,108 @@
+const a1 = document.querySelector('#a1')
+const a2 = document.querySelector('#a2')
+const a3 = document.querySelector('#a3')
+const b1 = document.querySelector('#b1')
+const b2 = document.querySelector('#b2')
+const b3 = document.querySelector('#b3')
+const c1 = document.querySelector('#c1')
+const c2 = document.querySelector('#c2')
+const c3 = document.querySelector('#c3')
+
+const isWin = () => {
+    if(a1.innerHTML && a1.innerHTML === a2.innerHTML && a2.innerHTML === a3.innerHTML) {
+        alert('Wygrywa' + " " + a1.innerHTML)
+        return true
+    }
+    if(b1.innerHTML && b1.innerHTML === b2.innerHTML && b2.innerHTML === b3.innerHTML) {
+        alert('Wygrywa' + " " + b1.innerHTML)
+        return true
+
+    }
+    if(c1.innerHTML && c1.innerHTML === c2.innerHTML && c2.innerHTML === c3.innerHTML) {
+        alert('Wygrywa' + " " + c1.innerHTML)
+        return true
+
+    }
+    if(a1.innerHTML && a1.innerHTML === b1.innerHTML && b1.innerHTML === c1.innerHTML) {
+        alert('Wygrywa' + " " + a1.innerHTML)
+        return true
+
+    }
+    if(a2.innerHTML && a2.innerHTML === b2.innerHTML && b2.innerHTML === c2.innerHTML) {
+        alert('Wygrywa' + " " + a2.innerHTML)
+        return true
+
+    }
+    if(a3.innerHTML && a3.innerHTML === b3.innerHTML && b3.innerHTML === c3.innerHTML) {
+        alert('Wygrywa' + " " + a3.innerHTML)
+        return true
+
+    }
+    if(a1.innerHTML && a1.innerHTML === b2.innerHTML && b2.innerHTML === c3.innerHTML) {
+        alert('Wygrywa' + " " + a1.innerHTML)
+        return true
+
+    }
+    if(a3.innerHTML && a3.innerHTML === b2.innerHTML && b2.innerHTML === c1.innerHTML) {
+        alert('Wygrywa' + " " + a3.innerHTML)
+        return true
+
+    }
+    return false
+}
+
+let counter = 0;
+
+const clickFieldHandler = (e) => {
+    if(!e.target.innerHTML) {
+        counter++;
+        if(counter % 2) {
+            e.target.innerHTML = "X"
+        }
+        else {
+            e.target.innerHTML = "O"
+        }
+    }
+    if (!isWin() && counter === 9) {
+        alert("REMIS")
+    }
+
+}
+
+a1.addEventListener ('click', clickFieldHandler)
+a2.addEventListener ('click', clickFieldHandler)
+a3.addEventListener ('click', clickFieldHandler)
+b1.addEventListener ('click', clickFieldHandler)
+b2.addEventListener ('click', clickFieldHandler)
+b3.addEventListener ('click', clickFieldHandler)
+c1.addEventListener ('click', clickFieldHandler)
+c2.addEventListener ('click', clickFieldHandler)
+c3.addEventListener ('click', clickFieldHandler)
+
+$("#a1").on("click", function(){ 
+    $("#a1").toggleClass("clicked");
+})
+$("#a2").on("click", function(){ 
+    $("#a2").toggleClass("clicked");
+})
+$("#a3").on("click", function(){ 
+    $("#a3").toggleClass("clicked");
+})
+$("#b1").on("click", function(){ 
+    $("#b1").toggleClass("clicked");
+})
+$("#b2").on("click", function(){ 
+    $("#b2").toggleClass("clicked");
+})
+$("#b3").on("click", function(){ 
+    $("#b3").toggleClass("clicked");
+})
+$("#c1").on("click", function(){ 
+    $("#c1").toggleClass("clicked");
+})
+$("#c2").on("click", function(){ 
+    $("#c2").toggleClass("clicked");
+})
+$("#c3").on("click", function(){ 
+    $("#c3").toggleClass("clicked");
+})
